c383fedcdf7d146aeeb49173f5fc459f90a4f3a2
Miguel Torres Perez <silverwolf1084@outlook.com>
2021-11-01 20:19:44 -0400

    Add project files.

---
+++ Milestone2/InventoryItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Author: Miguel Torres Perez
+// CST-150
+
+namespace Milestone2
+{
+    class InventoryItem
+    {
+        public string Flavor { get; private set; }
+        public double Price { get; private set; }
+        public int Quantity { get; private set; }
+        public string Description { get; private set; }
+
+        public InventoryItem(string flavor, double price, int quantity, string description)
+        {
+            this.Flavor = flavor;
+            this.Price = price;
+            this.Quantity = quantity;
+            this.Description = description;
+        }
+        public bool OutOfStock()
+        {
+            if(Quantity == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool AbleToBuy(int want)
+        {
+            if(Quantity >= want)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool BuyItem(int want)
+        {
+            if((want > 0) && (!OutOfStock()) && (AbleToBuy(want)))
+            {
+                Quantity = -want;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Milestone2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Author: Miguel Torres Perez
+// CST-150
+
+namespace Milestone2
+{
+    class InventoryManager
+
+    {
+        static void Main(string[] args)
+        {
+            InventoryItem item = new InventoryItem("Cherry", 1.50, 5, "Cherry Ice Cream");
+            Console.WriteLine("New Item: " + "\nItem Flavor: " + item.Flavor
+                + "\nItem Price: " + item.Price + "\nQuantity of item: "
+                + item.Quantity + "\nItem Description: " + item.Description);
+            Console.ReadKey();
+        }
+    }
+}
